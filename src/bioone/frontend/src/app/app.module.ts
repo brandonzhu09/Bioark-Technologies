dcb976b83c74021009d236d8411f009a15cac8e6
brandonzhu09 <brandonzhu123@gmail.com>
2024-06-17 12:05:45 -0400

    created preliminary navbar component

--- src/bioone/frontend/src/app/app.module.ts
+++ src/bioone/frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
