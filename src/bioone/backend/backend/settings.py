deab8844449febd0170439ef45d72ab4812f60ac
Brandon Zhu <brandonzhu123@gmail.com>
2024-09-13 11:51:00 -0400

    Modified settings

--- src/bioone/backend/backend/settings.py
+++ src/bioone/backend/backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-hytyud87s@4*51a3$=@%s^h7mtj81pl9t--@9)3^1@(d@8lvip'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
