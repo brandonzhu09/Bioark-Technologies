a7b92b0f01f6842642110317d146c5140c4a9e58
Brandon Zhu <brandonzhu123@gmail.com>
2024-10-01 08:52:51 -0400

    Updated urls typo

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -29,6 +29,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
-    path('products/', include('products.urls'),
+    path('products/', include('products.urls')),
     path('users/', include('users.urls')),
 ]
