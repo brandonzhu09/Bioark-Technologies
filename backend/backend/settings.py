58df750f57806844c2f6615baf3bfb63639c5b11
Brandon Zhu <brandonzhu123@gmail.com>
2024-12-16 19:36:26 -0500

    Removed verify email package

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'verify_email.apps.VerifyEmailConfig',
     'rest_framework',
     'users',
     'api',
