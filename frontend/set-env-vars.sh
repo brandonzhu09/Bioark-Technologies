ba7bff46b2bf333d93f8d06a6687acc184f84f5e
Brandon Zhu <brandonzhu123@gmail.com>
2025-01-03 15:31:26 -0500

    Added set env vars script

---
+++ frontend/set-env-vars.sh
@@ -0,0 +1,6 @@
+echo "export const environment = {
+  production: true,
+  apiBaseUrl: '',
+  salesTaxUrl: '$TAX_URL',
+  ninjasApiKey: '$NINJAS_API_KEY'
+};" > src/environment/environment.prod.ts
--- frontend/src/environment/environment.prod.ts
+++ frontend/src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: true,
-    apiBaseUrl: '',
-    salesTaxUrl: process.env["TAX_URL"],
-    ninjasApiKey: process.env["NINJAS_API_KEY"]
+  production: true,
+  apiBaseUrl: '',
+  salesTaxUrl: '',
+  ninjasApiKey: ''
 };
