ae50ecf84d3f1a5b6f1a05135b6fd8565de2a83e
Brandon Zhu <brandonzhu123@gmail.com>
2025-01-04 12:51:53 -0500

    Changed api base url

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ cd frontend
 
 echo "export const environment = {
   production: true,
-  apiBaseUrl: '',
+  apiBaseUrl: 'https://api.bioarktech.com',
   salesTaxUrl: '$TAX_URL',
   ninjasApiKey: '$NINJAS_API_KEY'
 };" > src/environment/environment.prod.ts
@@ -11,5 +11,3 @@ touch src/environment/environment.ts
 
 npm install
 npx ng build --configuration=production
-
-echo "/api/* https://bioarktech.com/api/:splat 200" > dist/frontend/browser/_redirects
