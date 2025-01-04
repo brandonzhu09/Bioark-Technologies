903fa1609dd0b6621b6da4ab21eb0d8d16f8e660
Brandon Zhu <brandonzhu123@gmail.com>
2025-01-04 15:05:21 -0500

    Edit build script

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 cd frontend 
 
+mkdir src/environment
+touch src/environment/environment.prod.ts
+touch src/environment/environment.ts
+
 echo "export const environment = {
   production: true,
   apiBaseUrl: 'https://api.bioarktech.com',
   salesTaxUrl: '$TAX_URL',
   ninjasApiKey: '$NINJAS_API_KEY'
 };" > src/environment/environment.prod.ts
-
-touch src/environment/environment.ts
 
 npm install
 npx ng build --configuration=production
