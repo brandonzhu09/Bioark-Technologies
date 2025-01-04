61f4e5ed1b3a36c74f4ccd0ead42b306560d7992
Brandon Zhu <brandonzhu123@gmail.com>
2025-01-04 12:03:41 -0500

    Add redirects file to api server

--- frontend/set-env-vars.sh
+++ frontend/set-env-vars.sh
@@ -5,4 +5,6 @@ echo "export const environment = {
   ninjasApiKey: '$NINJAS_API_KEY'
 };" > src/environment/environment.prod.ts
 
+echo "/api/* https://bioarktech.com/api/:splat 200" > dist/frontend/browser/_redirects
+
 touch src/environment/environment.ts
