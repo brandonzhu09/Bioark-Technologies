ad3a755bc8e3ba06e3fbaf9e1ae618c972be47bd
Brandon Zhu <brandonzhu123@gmail.com>
2025-03-02 11:02:47 -0500

    Added lego diagram in design mode

--- frontend/src/app/design-mode/design-diagram/design-diagram.component.ts
+++ frontend/src/app/design-mode/design-diagram/design-diagram.component.ts
@@ -10,11 +10,11 @@ export class DesignDiagramComponent {
   @Input() category: string = '';
   @Input() functionTypeName: string = '';
   @Input() structureTypeName: string = '';
-  @Input() promoterName: string = '';
-  @Input() proteinTagName: string = '';
-  @Input() fluoresceneMarkerName: string = '';
-  @Input() selectionMarkerName: string = '';
-  @Input() bacterialMarkerName: string = '';
+  @Input() promoterName: string = 'None';
+  @Input() proteinTagName: string = 'None';
+  @Input() fluoresceneMarkerName: string = 'None';
+  @Input() selectionMarkerName: string = 'None';
+  @Input() bacterialMarkerName: string = 'None';
   @Input() targetSequence: string = '';
 
 
