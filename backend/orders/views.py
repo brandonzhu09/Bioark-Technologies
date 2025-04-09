7fc68f7c0e3ec7cc5febb65a61d4124c7c36d151
Brandon Zhu <brandonzhu123@gmail.com>
2025-04-09 15:17:02 -0400

    Fix paypal env typo

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -30,8 +30,8 @@ import requests
 
 load_dotenv()
 
-PAYPAL_CLIENT_ID = os.getenv("PAYPAL_CLIENT_ID_DEV")
-PAYPAL_CLIENT_SECRET = os.getenv("PAYPAL_CLIENT_SECRET_DEV")
+PAYPAL_CLIENT_ID = os.getenv("PAYPAL_CLIENT_ID")
+PAYPAL_CLIENT_SECRET = os.getenv("PAYPAL_CLIENT_SECRET")
 PAYPAL_API_BASE = "https://api-m.paypal.com"
 DEBUG = os.getenv("DEBUG_FLAG")
 
